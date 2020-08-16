7ca5c6b86a5b5df67a645b9ab327e1ed7caa8390
Travis <travis>
2020-08-16 13:24:51 -0700

    add arm-eabi-4.8 toolchain

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export ARCH=arm
-export CROSS_COMPILE=$(pwd)/../PLATFORM/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-
+export CROSS_COMPILE=$(pwd)/lazy-prebuilt/bin/arm-eabi-
 mkdir output
 
 make -C $(pwd) O=output msm8916_sec_defconfig VARIANT_DEFCONFIG=msm8916_sec_gt510wifi_eur_defconfig SELINUX_DEFCONFIG=selinux_defconfig
